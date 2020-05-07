10c6ea17608384b5041533d04797bd16e36253b1
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2020-05-07 11:05:57 +0200

    update csv reader

--- read_csv_script.py
+++ read_csv_script.py
@@ -10,15 +10,20 @@ csv_reader = CsvReader('/home/salim/Bureau/11009101406003_apr 14_2010.csv')
 manual_rois = csv_reader.get_manual_rois()
 print(manual_rois)
 #Ligne par ligne tu peux recupérer un object qui decrit la ROI et que tu va pouvoir utiliser pour crée ta ROI
-print(csv_reader.convert_manual_row_to_object(manual_rois[0]))
+#print(csv_reader.convert_manual_row_to_object(manual_rois[0]))
 
 
+def get_ROIS(manual_rois):
+    ROIs = {}
+    for i in range(csv_reader.number_of_manual_roi):
+        ROIs[i+1] = csv_reader.convert_manual_row_to_object(manual_rois[i])
+    number_of_roi = list(ROIs.keys())
+    return ROIs, number_of_roi
 
-#avoir tous les ROIs dans un dict
-ROIs = {}
-for i in range(csv_reader.number_of_manual_roi):
-    ROIs[i+1] = csv_reader.convert_manual_row_to_object(manual_rois[i]) # {1 : roi 1 , 2 : roi 2 etc }
-number_of_roi = list(ROIs.keys()) # [1, 2, 3, 4] etc 
+ROIs , number_of_roi = get_ROIS(manual_rois) #pour avoir tous les rois dans un dict
+#ROIs = { 1 : roi 1, 2: roi 2 etc}
+
+
 
 
 #fonction pour mettre la liste des points en array N*2 pour matplotlib.patches.Polygon/Ellipse
@@ -33,6 +38,8 @@ def pointlist_to_pointarray (point_list):
 def create_closed_polygon(roi):
     points_array = pointlist_to_pointarray(roi['point_list']) #array nbr de points*2
     return matplotlib.patches.Polygon(points_array, closed = True)
+    
+    
 
 def create_elipse(roi):
     points_array = pointlist_to_pointarray(roi['point_list'])
@@ -51,33 +58,41 @@ np_array_3D = np.zeros((256, 256, 700))
 
 #dessiner le poly ou ellipse
 #ecrire fonction pour dessiner soit poly soit ellipse pour chaque ROI 
-first_ROI = ROIs[1] #premier ROI du dictionnaire ROIs
 
-if first_ROI['type'] == 'Polygon' : 
-    patch = create_closed_polygon(first_ROI)
-    xmin, ymin, xmax, ymax = min_max(patch)
-elif first_ROI['type'] == 'Elipse' : 
-    patch = create_elipse(first_ROI)
-    #aller chercher les xmin xmax ymin ymax des ellipses
+def draw_patches(roi):
+    if roi['type'] == 'Polygon' :
+        return create_closed_polygon(roi)
+    elif roi['type'] == 'Elipse' :
+        return create_elipse(roi)
 
 
 
-def min_max(patch): #return les min max des sommets du polygone
-    sommets = patch.get_xy()
-    x = []
-    y = []
-    for i in range(sommets.shape[0]):
-        x.append(sommets[i,0])
-        y.append(sommets[i,1])
-    return min(x), min(y), max(x), max(y)
+def min_max(roi): #return les min max des patches ellipse ou polygone
+    if roi['type'] == 'Polygon' :
+        points_array = pointlist_to_pointarray(roi['point_list'])
+        x = []
+        y= []
+        for i in range (points_array.shape[0]):
+            x.append(points_array[i, 0])
+            y.append(points_array[i, 1])
+        return min(x), min(y), max(x), max(y)
+    
+    elif roi['type'] == 'Elipse' :
+        points_array = pointlist_to_pointarray(roi['point_list'])
+        width = abs(points_array[0][0] - points_array[1][0]) #centre_x - est_x 
+        height = abs(points_array[0][1] - points_array[2][1]) #centre_y - nord_y
+        xmin = int(points_array[0][0] - width/2)
+        ymin = int(points_array[0][1] - height/2)
+        xmax = int(points_array[0][0] + width/2)
+        ymax =  int(points_array[0][1] + height/2)
+        return xmin, ymin, xmax, ymax
 
 
-#POUR ROI 1, 
-
-def mark_roi_in_slice(slice, patch, number_of_roi): #patch = ellipse ou epsilon
+def mark_roi_in_slice(roi, slice, patch, number_of_roi): #patch = ellipse ou polygone #slice = np array 256*256
+    xmin, ymin, xmax, ymax = min_max(roi)
     for i in range(xmin, xmax): 
         for j in range(ymin, ymax) : 
-            if patch.contains_point((i,j), radius = 0) : #si vrai alors changement 
+            if patch.contains_point([i,j], radius = 0) : #si vrai alors changement 
                 slice[i,j] =  number_of_roi # = 1,2,3 etc 
     return slice 
 
@@ -85,15 +100,12 @@ def mark_roi_in_slice(slice, patch, number_of_roi): #patch = ellipse ou epsilon
 
 
 
-#numero premiere et derniere slice ou appliquer
-first_slice = first_ROI['first_slice']
-last_slice = first_ROI['last_slice'] 
-
-#il faudrait les sortir pour les tous les ROIS du fichier csv 
+#ROIs = dictionnaire de dictionnaire 
+#boucle sur tous les ROIS du fichier 
 for number_roi in number_of_roi : 
-
-    for number_of_slices in range(first_slice, last_slice + 1) : 
-        np_array_3D[number_of_slices] = mark_roi_in_slice(np_array_3D[number_of_slices], patch, number_roi) #pour ROI 1
+    patch = draw_patches(ROIs[number_roi])
+    for number_of_slices in range(ROIs[number_roi]['first_slice'], ROIs[number_roi]['last_slice'] + 1 ) : 
+        np_array_3D[number_of_slices] = mark_roi_in_slice(ROIs[number_roi], np_array_3D[number_of_slices], patch, number_roi) 
 
 
 
