aa0c4299deba2f82824ff8a03be93a256df0b8e6
Salim Kanoun <salim.kanoun@gmail.com>
2020-05-10 14:44:40 +0200

    ref

--- read_csv_script2.py
+++ read_csv_script2.py
@@ -10,10 +10,9 @@ manual_rois = csv_reader.get_manual_rois()
 for manual_roi in manual_rois:
     roi_object = csv_reader.convert_manual_row_to_object(manual_roi)
     print(roi_object)
+    roi_factory1 = RoiFactory(roi_object, (256,256,512), 1)
+    roi1 = roi_factory1.read_roi()
+    mask1 = roi1.calculateMaskPoint()
+    print(mask1)
 #roi_object = csv_reader.convert_nifti_row_to_list_point(manual_rois[0])
 
-
-print(roi_object)
-roi_factory1 = RoiFactory(roi_object, (256,256,512), 1)
-roi1 = roi_factory1.read_roi()
-mask1 = roi1.calculateMaskPoint()
