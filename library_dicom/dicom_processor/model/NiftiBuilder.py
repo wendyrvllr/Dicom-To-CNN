80fdd9205db3c12cf78554dfb39c92918dfd5d1b
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2020-05-15 19:29:04 +0200

    change axis , axial nifti

--- library_dicom/dicom_processor/model/NiftiBuilder.py
+++ library_dicom/dicom_processor/model/NiftiBuilder.py
@@ -1,5 +1,6 @@
 from library_dicom.dicom_processor.enums.SopClassUID import *
 import SimpleITK as sitk
+import numpy as np
 
 class NiftiBuilder:
     """BuildNifti File using a Series Object
@@ -9,7 +10,7 @@ class NiftiBuilder:
         self.series = series
     
     def save_nifti(self, filename):
-        sitk_img = sitk.GetImageFromArray( self.series.get_numpy_array() )
+        sitk_img = sitk.GetImageFromArray( np.transpose(self.series.get_numpy_array(), (2,0,1) ))
         original_pixel_spacing = self.series.instance_array[0].get_pixel_spacing()
         print(self.series.instance_array[0].get_image_orientation())
         sitk_img.SetDirection( (1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0) )
