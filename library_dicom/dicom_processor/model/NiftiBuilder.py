eea26b1bfb9bfb5f9a4e3a5167d977059ebfad6c
Salim Kanoun <salim.kanoun@gmail.com>
2020-06-15 14:52:14 +0200

    nifti_builder

--- library_dicom/dicom_processor/model/NiftiBuilder.py
+++ library_dicom/dicom_processor/model/NiftiBuilder.py
@@ -12,7 +12,7 @@ class NiftiBuilder:
     def save_nifti(self, filename):
         sitk_img = sitk.GetImageFromArray( np.transpose(self.series.get_numpy_array(), (2,0,1) ))
         original_pixel_spacing = self.series.instance_array[0].get_pixel_spacing()
-        print(self.series.instance_array[0].get_image_orientation())
+        #print(self.series.instance_array[0].get_image_orientation())
         sitk_img.SetDirection( (1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0) )
         sitk_img.SetOrigin( self.series.instance_array[0].get_image_position() )
         sitk_img.SetSpacing( (original_pixel_spacing[0], original_pixel_spacing[1], self.series.get_z_spacing()) )
