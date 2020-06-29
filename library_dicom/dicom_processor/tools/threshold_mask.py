502ad7c185973a3e60617f1b55e8753e08c90607
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2020-06-29 11:37:22 +0200

    threshold_mask

---
+++ library_dicom/dicom_processor/tools/threshold_mask.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+def get_suv_max(nifti_array, list_points, flip = False):
+    list_suv = []
+    if list_points != [] :
+        for point in list_points : 
+            if flip == False : 
+                list_suv.append(nifti_array[point[1], point[0], point[2]])
+
+            else : list_suv.append(nifti_array[point[1], point[0], (slice- 1) - point[2]])
+
+        return np.max(list_suv)
+    else : return 0.0 #pas de suv max
+
+
+
+
+def threshold_mask(mask_4D, threshold, details_rois, nifti_array,  flip = False):
+    number_of_roi = mask_4D.shape[3]
+
+    slice = mask_4D.shape[2]
+    if flip == False : 
+        for roi in number_of_roi : 
+            list_points = details_rois[roi + 1]['list_point']
+            suv_max = get_suv_max(nifti_array, list_points, flip = False)
+
+            #GET THRESHOLD
+            if "%" in threshold : 
+                threshold = float(threshold.strip("%"))/100 * suv_max
+            else : 
+                threshold = float(threshold)
+
+            for point in list_points :
+                if nifti_array[point[1], point[0], point[2]] <= threshold :
+                    mask_4D[point[1], point[0], point[2], roi] = 0
+
+        return mask_4D
+
+    else :  
+        for roi in number_of_roi : 
+            list_points = details_rois[roi + 1]['list_point']
+            suv_max = get_suv_max(nifti_array, list_points, flip = True)
+
+            #GET THRESHOLD
+            if "%" in threshold : 
+                threshold = float(threshold.strip("%"))/100 * suv_max
+            else : 
+                threshold = float(threshold)
+
+            for point in list_points :
+                if nifti_array[point[1], point[0], (slice- 1) - point[2]] <= threshold :
+                    mask_4D[point[1], point[0], (slice- 1) - point[2], roi] = 0
+
+        return mask_4D               
+
+            
+       
+
+            
+
+            
