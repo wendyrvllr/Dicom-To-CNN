337c134675cfbae57bc513f68226d0f6bbc342d5
Salim Kanoun <salim.kanoun@gmail.com>
2020-06-30 11:54:30 +0200

    threshold_mask

--- library_dicom/dicom_processor/tools/threshold_mask.py
+++ library_dicom/dicom_processor/tools/threshold_mask.py
@@ -1,13 +1,11 @@
 import numpy as np
+#on utilise le mask_4D déjà flippé, donc coordonées déjà flippé aussi
 
-def get_suv_max(nifti_array, list_points, flip = False):
+def get_suv_max(nifti_array, list_points):
     list_suv = []
     if list_points != [] :
         for point in list_points : 
-            if flip == False : 
-                list_suv.append(nifti_array[point[1], point[0], point[2]])
-
-            else : list_suv.append(nifti_array[point[1], point[0], (slice- 1) - point[2]])
+            list_suv.append(nifti_array[point[1], point[0], point[2]])
 
         return np.max(list_suv)
     else : return 0.0 #pas de suv max
@@ -15,46 +13,28 @@ def get_suv_max(nifti_array, list_points, flip = False):
 
 
 
-def threshold_mask(mask_4D, details_rois, nifti_array,  flip = False):
+def threshold_mask(mask_4D, details_rois, nifti_array):
 
     number_of_roi = mask_4D.shape[3]
 
-    slice = mask_4D.shape[2]
-    if flip == False : 
-        for roi in range(number_of_roi) : 
-            list_points = details_rois[roi + 1]['list_points']
-            suv_max = get_suv_max(nifti_array, list_points, flip = False)
+    for roi in range(number_of_roi) : 
+        list_points = details_rois[roi + 1]['list_points']
+        suv_max = get_suv_max(nifti_array, list_points)
 
             #GET THRESHOLD
-            threshold = details_rois['SUVlo']
-            if "%" in threshold : 
-                threshold = float(threshold.strip("%"))/100 * suv_max
-            else : 
-                threshold = float(threshold)
+        threshold = details_rois['SUVlo']
+        if "%" in threshold : 
+            threshold = float(threshold.strip("%"))/100 * suv_max
+        else : 
+            threshold = float(threshold)
 
-            for point in list_points :
-                if nifti_array[point[1], point[0], point[2]] <= threshold :
-                    mask_4D[point[1], point[0], point[2], roi] = 0
+        for point in list_points :
+            if nifti_array[point[1], point[0], point[2]] <= threshold :
+                mask_4D[point[1], point[0], point[2], roi] = 0
 
-        return mask_4D
+    return mask_4D
 
-    else :  
-        for roi in range(number_of_roi) : 
-            list_points = details_rois[roi + 1]['list_points']
-            suv_max = get_suv_max(nifti_array, list_points, flip = True)
 
-            #GET THRESHOLD
-            threshold = details_rois['SUVlo']
-            if "%" in threshold : 
-                threshold = float(threshold.strip("%"))/100 * suv_max
-            else : 
-                threshold = float(threshold)
-
-            for point in list_points :
-                if nifti_array[point[1], point[0], (slice- 1) - point[2]] <= threshold :
-                    mask_4D[point[1], point[0], (slice- 1) - point[2], roi] = 0
-
-        return mask_4D               
 
             
        
