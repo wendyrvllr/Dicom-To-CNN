b1edae8291743460d91eb97e205dd2150fa53840
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2020-07-30 16:49:33 +0200

    Mask

--- library_dicom/post_processing/Mask.py
+++ library_dicom/post_processing/Mask.py
@@ -5,6 +5,7 @@ from radiomics.featureextractor import RadiomicsFeatureExtractor
 import matplotlib.pyplot as plt
 import scipy as sc
 from sklearn import mixture
+from pandas import DataFrame
 
 
 class Mask: 
@@ -18,6 +19,7 @@ class Mask:
         self.shape_mask = self.mask.shape
         self.binary_mask = self.get_binary_mask()
         self.shape_binary_mask = self.binary_mask.shape 
+        
     
 
 
@@ -91,60 +93,6 @@ class Mask:
             #dict[label] = subdict
         #return dict
 
-    def gaussien_mixture_model(self, dict) : 
-        parameters = {}
-        for key in range(1, len(dict) + 1) : 
-            X_train = np.asarray(dict[key]['suv_values']).reshape((-1,1))
-            subdict = {}
-
-            #find best parameters for model 
-            lowest_bic = np.infty
-            bic = []
-            n_components_range = range(1, 8)
-            cv_types = ['spherical', 'tied', 'diag', 'full']
-            for cv_type in cv_types : 
-                for n_components in n_components_range : 
-                    gmm = mixture.GaussianMixture(n_components=n_components, covariance_type=cv_type, random_state = 0)
-                    gmm.fit(X_train)
-
-                    bic.append(gmm.bic(X_train))
-                    if bic[-1] < lowest_bic : 
-                        lowest_bic = bic[-1]
-                        best_gmm = gmm 
-                        best_n_components = n_components
-                        best_cv_type = cv_type
-                        mean = best_gmm.means_
-                        covariance = best_gmm.covariances_
-
-            subdict['best_n_components'] = best_n_components
-            subdict['best_cv_type'] = best_cv_type
-            subdict['mean'] = mean
-            subdict['covariances'] = covariance
-            parameters[key] = subdict
-
-
-            #train model with best paramaters
-
-            gmm_x = np.linspace(0, np.max(X_train), X_train.shape[0])
-            gmm_y = np.exp(best_gmm.score_samples(gmm_x.reshape(-1,1)))
-
-
-            plt.hist(dict[key]['suv_values'], bins = 'fd', normed = True, alpha = 0.6, label = 'suv_value')
-            plt.xlabel('suv_value')
-            plt.ylabel('frequency')
-            
-            plt.plot(gmm_x, gmm_y, label = 'gmm model', color = 'mediumvioletred', lw = 2)
-            plt.title('roi :{}, n_components : {} , cv_type : {}'.format(key, best_n_components, best_cv_type))
-            plt.legend()
-            plt.show()
-
-        return parameters
-
-
-    #def get_volume_label(self, number_pixel):
-        #volume_pixel = self.mask_spacing[0] * self.mask_spacing[1] * self.mask_spacing[2]
-        #return volume_pixel * number_pixel * 10**(-3)
-
     def extract_features(self, pet_img, mask_img):
         if pet_img.GetSize() != mask_img.GetSize() or pet_img.GetSpacing() != mask_img.GetSpacing() or pet_img.GetDirection() != mask_img.GetDirection() or pet_img.GetOrigin() != mask_img.GetOrigin() : 
             raise Exception ("Not same origin, spacing, direction or size, different img")
@@ -214,7 +162,109 @@ class Mask:
             return dict     
 
 
+    def gaussien_mixture_model(self, dict) : 
+        parameters = {}
+        for key in range(1, len(dict) + 1) : 
+            X_train = np.asarray(dict[key]['suv_values']).reshape((-1,1))
+
+            subdict = {}
+
+            #find best parameters for model 
+            lowest_bic = np.infty
+            bic = []
+            n_components_range = range(1, 8)
+            cv_types = ['spherical', 'tied', 'diag', 'full']
+            for cv_type in cv_types : 
+                for n_components in n_components_range : 
+                    gmm = mixture.GaussianMixture(n_components=n_components, covariance_type=cv_type, random_state = 0)
+                    gmm.fit(X_train)
+
+                    
+
+                    bic.append(gmm.bic(X_train))
+                    if bic[-1] < lowest_bic : 
+                        lowest_bic = bic[-1]
+                        best_gmm = gmm 
+                        best_n_components = n_components
+                        best_cv_type = cv_type
+                        #mean = best_gmm.means_
+                        #covariance = best_gmm.covariances_
+                        #weights = best_gmm.weights_
+
+                        labels = best_gmm.predict(X_train) #ndarray
+                        
+
+
+            subdict['best_n_components'] = best_n_components
+            subdict['best_cv_type'] = best_cv_type
+            subdict['label'] = labels
+
+            parameters[key] = subdict
+
+            #train model with best paramaters
+
+            gmm_x = np.linspace(0, np.max(X_train), X_train.shape[0])
+            gmm_y = np.exp(best_gmm.score_samples(gmm_x.reshape(-1,1)))
+
+
+            plt.hist(dict[key]['suv_values'], bins = 'fd', normed = True, alpha = 0.6, label = 'suv_value')
+            plt.xlabel('suv_value')
+            plt.ylabel('frequency')
+            
+            plt.plot(gmm_x, gmm_y, label = 'gmm model', color = 'mediumvioletred', lw = 2)
+            plt.title('roi :{}, n_components : {} , cv_type : {}'.format(key, best_n_components, best_cv_type))
+            plt.legend()
+            plt.show()
+
+        return parameters
+
+
+    def get_index_cluster(self, labels, n_components): #for 1 roi 
+        liste = []
+        for i in range(n_components) : 
+            liste.append(np.where(labels == i))
+
+        return liste 
 
 
 
+    def get_new_clusters(self, parameters, features):
+        number_of_roi = len(parameters)
+        new_clusters = []
+        for key in range(1, number_of_roi + 1) : 
+            x,y,z = features[key]['coordonate']
+            liste_new_labels = self.get_index_cluster(parameters[key]['label'], parameters[key]['best_n_components'])
+            for liste in liste_new_labels : 
+                subliste = []
+                for index in liste[0] : 
+                    subliste.append([x[index], y[index], z[index]])
 
+                new_clusters.append(subliste)
+
+        return new_clusters
+
+    def get_number_total_of_roi(self, paramaters) : 
+        number_of_roi = len(paramaters)
+        number_total_of_roi = 0
+        for key in range(1, number_of_roi + 1) : 
+            new_labels = self.get_index_cluster(paramaters[key]['label'], paramaters[key]['best_n_components'])
+            number_total_of_roi += len(new_labels)
+
+        return np.arange(1, number_total_of_roi+1)
+
+
+    def get_new_labelled_mask(self, paramaters, features):
+        labelled_mask = np.zeros((self.shape_binary_mask[0], self.shape_binary_mask[1], self.shape_binary_mask[2]))
+        liste_new_labels = self.get_number_total_of_roi(paramaters)
+        coordonate_new_clusters = self.get_new_clusters(paramaters, features)
+        for liste, label in zip(coordonate_new_clusters, liste_new_labels) :
+            for index in liste : 
+                labelled_mask[index[0], index[1], index[2]] = label
+
+        return labelled_mask
+
+
+        
+    #def get_volume_label(self, number_pixel):
+        #volume_pixel = self.mask_spacing[0] * self.mask_spacing[1] * self.mask_spacing[2]
+        #return volume_pixel * number_pixel * 10**(-3)
