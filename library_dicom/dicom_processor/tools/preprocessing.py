1d774d41c2ef20fc3ede8a658f41ac20868b1087
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2020-08-10 11:24:23 +0200

    pre_process

--- library_dicom/dicom_processor/tools/preprocessing.py
+++ library_dicom/dicom_processor/tools/preprocessing.py
@@ -25,9 +25,10 @@ def resample_CT(ct_img, new_origin):
     target_direction = (1,0,0,0,1,0,0,0,1)
     shape = ct_img.GetSize()
     target_shape = (128, 128, 255) 
-    factor = shape[2] / target_shape[2]
+    factor_z = shape[2] / target_shape[2]
+    factor_x_y = shape[0] / target_shape[0]
     spacing = ct_img.GetSpacing()
-    target_voxel_spacing = (spacing[0]*4, spacing[1]*4, spacing[2] * factor) #mm
+    target_voxel_spacing = (spacing[0]*factor_x_y, spacing[1]*factor_x_y, spacing[2] * factor_z) #mm
     transformation = sitk.ResampleImageFilter()
     transformation.SetOutputDirection(target_direction)
     transformation.SetOutputSpacing(target_voxel_spacing)
