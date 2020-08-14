523f9efa8a9659eb58506bcb6c19f58f9bf896a4
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2020-08-14 13:02:16 +0200

    post_processing

--- library_dicom/dicom_processor/tools/create_mip.py
+++ library_dicom/dicom_processor/tools/create_mip.py
@@ -26,7 +26,7 @@ def create_gif(filenames, duration, path_gif):
     
         return None
 
-def mip_imshow(numpy_array, angle, cmap) :
+def mip_imshow(numpy_array, angle, cmap, type) :
     numpy_array = np.transpose(np.flip(numpy_array, axis = 2), (2,1,0)) #coronal
 
     vol_angle = scipy.ndimage.interpolation.rotate(numpy_array , angle , reshape=False, axes = (1,2))
@@ -35,9 +35,14 @@ def mip_imshow(numpy_array, angle, cmap) :
     f = plt.figure(figsize=(10,10))
     axes = plt.gca()
     axes.set_axis_off()
+    if type == 'mask' : 
+        plt.imshow(MIP, cmap = cmap)
+        plt.show()
+    else : 
+        plt.imshow(MIP, cmap = cmap, vmax = 5.0)
+        plt.show()
 
-    plt.imshow(MIP, cmap = cmap)
-    plt.show()
+
 
 def mip_imshow_4D(mask, angle, cmap) : 
     liste = []
@@ -159,9 +164,40 @@ def create_mip_gif(numpy_array, path_image, study_uid, type, cmap, borne_max):
 
 
 
+def transparent_1(myimage) : 
+ 
+    img = Image.open(myimage) 
+    img = img.convert("RGBA")
+    datas = img.getdata()
+ 
+    newData = []
+    for item in datas:
+        #print(item)
+        #if item[0] > 200 and item[1] > 200 and item[2] > 200:
+        if item[0] < 88 and item[1] < 88  and item[2] < 88:
+            newData.append((0, 0, 0, 0))
+        else:
+            newData.append(item)
+ 
+    img.putdata(newData)
+    img.save(myimage, "PNG") 
+    return(myimage)
 
 
-def create_mip_blend_gif(numpy_array_mask, numpy_array_tep, blended_directory, study_uid, cmap_mask, alpha):
+def superposer_img(mask,background, angle): # ft = front , bg = background
+ 
+    mask_filename = mask.split('.')[0]
+    mask = Image.open(mask, 'r')
+    #filename1 = background
+    background = Image.open(background, 'r')
+    text_img = Image.new('RGBA', background.size , (0, 0, 0, 0))
+    text_img.paste(background, (0,0))
+    text_img.paste(mask, (0,0), mask=mask)
+    text_img.save(mask_filename+'sup'+str(int(angle))+'.png', format="png")
+    return mask_filename+'sup'+str(int(angle))+'.png'
+
+
+def create_mip_superpose_gif(numpy_array_mask, numpy_array_tep, directory, study_uid, cmap_mask):
     """return a gif of a numpy_array MIP 
 
     """
@@ -176,24 +212,27 @@ def create_mip_blend_gif(numpy_array_mask, numpy_array_tep, blended_directory, s
     
     angles = np.linspace(0, 360, number_images)
     for angle in angles:
-        path_tep = mip_projection(numpy_array_tep, angle, blended_directory, study_uid,'tep', cmap='Greys', borne_max = 5)
+        path_tep = mip_projection(numpy_array_tep, angle, directory, study_uid,'tep', cmap='Greys', borne_max = 5)
         angle_filenames_tep.append(path_tep)
 
-        path_mask = mip_projection(numpy_array_mask, angle, blended_directory, study_uid,'mask', cmap = cmap_mask, borne_max = 1)
+        path_mask = mip_projection(numpy_array_mask, angle, directory, study_uid,'mask', cmap = cmap_mask, borne_max = 1)
         angle_filenames_mask.append(path_mask)
 
-        tep_img = Image.open(path_tep)
-        mask_img = Image.open(path_mask)
 
-        blended = Image.blend(tep_img, mask_img, alpha = alpha)
-        blended.save(blended_directory+'\\'+'blended_'+str(int(angle))+".png")
-        angle_filenames.append(blended_directory+'\\'+'blended_'+str(int(angle))+".png")
+        #mask transparant
+        new_path_mask = transparent_1(path_mask)
 
+        #superposition
 
+        angle_filenames.append(superposer_img(new_path_mask, path_tep, angle))
         os.remove(path_tep)
-        os.remove(path_mask)
+        os.remove(new_path_mask)
 
-    create_gif(angle_filenames, duration, blended_directory)
+
+    create_gif(angle_filenames, duration, directory)
+
+    for path in angle_filenames : 
+        os.remove(path)
        
     
     return None
