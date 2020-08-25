7326b54f2a275487aa21f2b0232530c0e3068130
wendyrvllr <wendyrevailler@yahoo.fr>
2020-08-25 17:45:31 +0200

    seuillage du mask tools

--- library_dicom/dicom_processor/tools/threshold_mask.py
+++ library_dicom/dicom_processor/tools/threshold_mask.py
@@ -1,5 +1,26 @@
 import numpy as np
 
+#with any threshold
+
+def get_threshold_matrix(mask, pet_array, number_of_roi, threshold) : #0.41
+    for i in range(1, number_of_roi + 1):
+        suv_max = []
+        x,y,z = np.where(mask == i)
+        for j in range(len(x)):
+            suv_max.append(pet_array[x[j], y[j], z[j]])
+
+
+        seuil = threshold * np.max(suv_max)
+
+        a,b,c = np.where(pet_array <= seuil)
+        for k in range(len(x)) :
+            mask[a[k], b[k], c[k]] = 0
+        
+    return mask
+
+
+#from csv
+
 def get_suv_max(nifti_array, list_points):
     list_suv = []
     if list_points != [] :
@@ -8,8 +29,6 @@ def get_suv_max(nifti_array, list_points):
 
         return np.max(list_suv)
     else : return 0.0 #pas de suv max
-
-
 
 
 def threshold_mask(mask_4D, details_rois, nifti_array):
