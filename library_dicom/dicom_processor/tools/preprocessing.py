28e3ed33950883f20e7c2a605803b686ebf004e5
wendyrvllr <wendyrevailler@yahoo.fr>
2020-09-16 16:22:54 +0200

    proprocess ct for classification cnn

--- library_dicom/dicom_processor/tools/preprocessing.py
+++ library_dicom/dicom_processor/tools/preprocessing.py
@@ -42,7 +42,7 @@ def normalize_CT(ct_path):
     #return transformation.Execute(ct_img)
 
 
-def resample_CT(ct_img):
+def resample_CT(ct_img, directory_path, study_uid):
     #ct_img = read_nifti_ct(ct_path)
     spacing = ct_img.GetSpacing()
     origin = ct_img.GetOrigin()
@@ -50,6 +50,9 @@ def resample_CT(ct_img):
     #generate mip array
     array = sitk.GetArrayFromImage(ct_img)
     mip = np.amax(array, axis=1)
+    #print("mip shape :", mip.shape)
+    #plt.imshow(np.rot90(mip, k =2))
+    #plt.show()
 
     #generate mip image 
     mip_img = sitk.GetImageFromArray(mip)
@@ -61,13 +64,17 @@ def resample_CT(ct_img):
     spacing_x = 700/256 #mm
     spacing_y = 2000/1024 #mm
     mip_img_size = mip_img.GetSize() #pixel
+    #print("mip_img_size :", mip_img_size)
     mip_img_spacing = mip_img.GetSpacing() #mm
+    #print("mip_img_spacing :", mip_img_spacing)
     mip_img_origin = mip_img.GetOrigin() #mm
     mip_img_direction = mip_img.GetDirection() 
     
 
     true_x = mip_img_size[0] * mip_img_spacing[0] #mm
     true_y = mip_img_size[1] * mip_img_spacing[1] #mm 
+    #print("largeur :", true_x)
+    #print("hauteur :", true_y)
 
     new_size_x = int((true_x * 256) / 700) #pixel
     new_size_y = int((true_y * 1024) / 2000) #pixel
@@ -85,15 +92,24 @@ def resample_CT(ct_img):
 
     result = sitk.GetArrayFromImage(new_img)
     result = np.rot90(result, k=2)
-    #print(result.shape)
+    print("result :", result.shape)
     center = [512, 127]
     x = int(result.shape[0]/2)
     y = int(result.shape[1]/2)
+    #print(x)
+    #print(y)
     sommet_x = center[0] - x 
     sommet_y = center[1] - y 
-    new_array = np.zeros((1024, 256))
-    new_array[sommet_x:sommet_x + new_size_y, sommet_y:sommet_y + new_size_x] = result
 
+    #print(sommet_x)
+    #print(sommet_y)
+    new_array = np.zeros((1024, 256))
+    if result.shape[1] != 256 : 
+        new_array[sommet_x:sommet_x + result.shape[0], sommet_y:sommet_y + result.shape[1]] = result
+    else : 
+        new_array[sommet_x:sommet_x + result.shape[0], 0:256] = result
+
+    save_mip_nifti(new_array, mip_img_origin, mip_img_direction, mip_img_spacing, directory_path, study_uid)
     return new_array
 
 def save_mip(mip, directory_path, study_uid):
@@ -108,3 +124,13 @@ def save_mip(mip, directory_path, study_uid):
     f.savefig(full_filename, bbox_inches='tight')
     plt.close()        
     return full_filename
+
+def save_mip_nifti(mip, origin, direction, spacing, directory_path, study_uid):
+    img = sitk.GetImageFromArray(mip)
+    img.SetOrigin(origin)
+    img.SetDirection(direction)
+    img.SetSpacing(spacing)
+    filename = directory_path+'/'+study_uid+'_mip_ct_nifti_'+".nii"
+    sitk.WriteImage(img, filename)
+
+    return None
