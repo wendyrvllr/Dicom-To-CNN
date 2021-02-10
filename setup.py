58f2006785b12b80ac443cfd74003ffb56e39f6a
wendy <wendyrevailler@yahoo.fr>
2021-02-10 12:26:30 +0100

    publish package

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ setuptools.setup(
     description="Python Library to handle Input / Output conversion in Dicom <=> Convolutional Neural Network ",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
+    url = 'https://github.com/wendyrvllr/Dicom-To-CNN',   # Provide either the link to your github or to your website
+   download_url = 'https://github.com/wendyrvllr/Dicom-To-CNN/archive/v1.0.tar.gz',
     packages=setuptools.find_packages(),    # List of all python modules to be installed
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7, 3.8",
+        "License :: GPL-3.0 License ",
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
     py_modules=["dicom-to-cnn"],             # Name of the python package
-    package_dir={'':'Dicom-To-CNN'},     # Directory of the source code of the package
+    package_dir={'':'Dicom-To-CNN/library_dicom'},     # Directory of the source code of the package
     install_requires=[]                     # Install other dependencies if any
 )
