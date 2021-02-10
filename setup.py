658c2f75035a9015f5676ee9ae794c6bc56a53d0
wendy <wendyrevailler@yahoo.fr>
2021-02-10 11:35:17 +0100

    publish package

--- setup.py
+++ setup.py
@@ -4,3 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
+setuptools.setup(
+    name="Dicom-To-CNN",                     # This is the name of the package
+    version="0.0.1",                        # The initial release version
+    author="",                     # Full name of the author
+    description="Python Library to handle Input / Output conversion in Dicom <=> Convolutional Neural Network ",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["dicom-to-cnn"],             # Name of the python package
+    package_dir={'':'Dicom-To-CNN'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
+)
