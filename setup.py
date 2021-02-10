004db0622102cd879086cfb673aef466c6182600
wendy <wendyrevailler@yahoo.fr>
2021-02-10 12:53:19 +0100

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-    name="Dicom-To-CNN",                     # This is the name of the package
+    name="DicomToCNN",                     # This is the name of the package
     version="0.0.1",                        # The initial release version
     author="",                     # Full name of the author
     description="Python Library to handle Input / Output conversion in Dicom <=> Convolutional Neural Network ",
@@ -21,7 +21,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["dicom-to-cnn"],             # Name of the python package
+    py_modules=["dicomtocnn"],             # Name of the python package
     #package_dir={'':'Dicom-To-CNN'},     # Directory of the source code of the package
     install_requires=[]                     # Install other dependencies if any
 )
