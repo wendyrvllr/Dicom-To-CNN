8d514e8344f12a5a07d2e617a36f25ee75c5a88e
wendyrvllr <wendyrevailler@yahoo.fr>
2021-03-09 16:27:18 +0100

    update

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dicom_to_cnn",                     # This is the name of the package
-    version="0.8",                        # The initial release version
+    version="0.9",                        # The initial release version
     author="",                     # Full name of the author
     description="Python Library to handle Input / Output conversion in Dicom <=> Convolutional Neural Network ",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
     url = 'https://github.com/wendyrvllr/Dicom-To-CNN',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/wendyrvllr/Dicom-To-CNN/archive/7.tar.gz',
+    download_url = 'https://github.com/wendyrvllr/Dicom-To-CNN/archive/8.tar.gz',
     packages=setuptools.find_packages(),    # List of all python modules to be installed
-    #packages=["library_dicom"],
     classifiers=[
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
