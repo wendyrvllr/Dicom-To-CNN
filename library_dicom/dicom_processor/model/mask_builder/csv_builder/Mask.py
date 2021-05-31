188cc1a026068b95be748a9c9f2aa2cdf901788e
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2021-05-31 17:59:19 +0200

    Mask

--- library_dicom/dicom_processor/model/mask_builder/csv_builder/Mask.py
+++ library_dicom/dicom_processor/model/mask_builder/csv_builder/Mask.py
@@ -1,33 +1,37 @@
 import numpy as np 
 import SimpleITK as sitk 
+from library_dicom.dicom_processor.model.reader.SeriesPT import SeriesPT
 
 
 class Mask: 
-    """A class to generate mask nifti 
+    """A class to generate mask nifti associated to a PET Series
     """
 
-    def __init__(self, mask_array, serie_pet_object):
+    def __init__(self, mask_array:np.ndarray, serie_pet_object:SeriesPT):
         """constructor
 
         Args:
-            mask_array ([ndarray]): [mask ndarray]
+            mask_array ([np.ndarray]): [mask ndarray of shape [x,y,z,c]]
             serie_pet_object ([SeriesPT]): [SeriesPT object]
         """
         self.mask_array = mask_array 
         self.serie_pet_object = serie_pet_object 
 
 
-    def export_nifti(self, file_path):
+    def export_nifti(self, file_path:str):
+        """method to export/save ndarray of mask to nifti format
+
+        Args:
+            file_path (str): [directory+filename of the nifti]
+        """
         instance_array = self.serie_pet_object.get_instances_ordered()
         number_of_roi = self.mask_array.shape[3] #tjrs de taille 4, si une seule roi=> dernier channel =1
         slices = []
         for number_roi in range(number_of_roi) : 
-            slices.append(np.transpose(self.mask_array[:,:,:,number_roi], (2,0,1)))
-                
+            slices.append(np.transpose(self.mask_array[:,:,:,number_roi], (2,0,1)))  
         mask_4D = np.stack(slices, axis = 3)
         sitk_img = sitk.GetImageFromArray(mask_4D, isVector = True)
         sitk_img = sitk.Cast(sitk_img, sitk.sitkVectorUInt8)
-            
         original_pixel_spacing = instance_array[0].get_pixel_spacing()
         original_direction = instance_array[0].get_image_orientation()
         sitk_img.SetDirection( (float(original_direction[0]), float(original_direction[1]), float(original_direction[2]), 
