341d2069e4511a87f2c73e8d53379be3f63fde7d
wendyrvllr <62479340+wendyrvllr@users.noreply.github.com>
2021-06-04 15:31:41 +0200

    clean

---
+++ library_dicom/model/segmentation/nifti/Nifti_Writer.py
@@ -0,0 +1,26 @@
+import SimpleITK as sitk 
+import os
+from library_dicom.model.reader.SeriesPT import SeriesPT
+
+class Nifti_Writer:
+    """a class to convert a np.ndarray to a sitk.Image  
+    """
+
+    def __init__(self, img_mask:sitk.Image):
+        """constructor
+
+        Args:
+            img_mask (sitk.Image) : [sitk.Image of shape (x,y,z)]
+        """
+        self.img_mask= img_mask
+
+    def save_file(self, filename:str, directory_path:str):
+        """method to save the new nifti file
+
+        Args:
+            filename (str): [name of the new nifti file]
+            directory_path (str): [directory's path where to save the new nifti file]
+        
+        """
+        
+        sitk.WriteImage(self.mask_img, os.path.join(directory_path, filename))
