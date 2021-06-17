11b6b5de2072818ce8c76d0bbda161f565b0020a
wendyrvllr <wendyrevailler@yahoo.fr>
2021-06-17 11:20:07 +0200

    change license + documentation

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dicom_to_cnn",                     # This is the name of the package
-    version="0.68",                        # The initial release version
+    version="0.69",                        # The initial release version
     author="",                     # Full name of the author
     description="Python Library to handle Input / Output conversion in Dicom <=> Convolutional Neural Network ",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
     url = 'https://github.com/wendyrvllr/Dicom-To-CNN',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/wendyrvllr/Dicom-To-CNN/archive/refs/tags/67.tar.gz',
+    download_url = 'https://github.com/wendyrvllr/Dicom-To-CNN/archive/refs/tags/68.tar.gz',
     packages=setuptools.find_packages(),    # List of all python modules to be installed
     classifiers=[
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.7',                # Minimum version requirement of the package
