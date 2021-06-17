06341bf412f9f8fe4de1a50002d90ce713738743
wendyrvllr <wendyrevailler@yahoo.fr>
2021-06-17 11:22:25 +0200

    license to MIT

--- documentation/conf.py
+++ documentation/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Salim Kanoun, Wendy Revailler'
 author = 'Salim Kanoun, Wendy Revailler'
 
 # The full version, including alpha/beta/rc tags
-release = '0.68'
+release = '0.70'
 
 
 # -- General configuration ---------------------------------------------------
