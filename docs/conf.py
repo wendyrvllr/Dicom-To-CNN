fb79471e62798059371c791a8010b2caf1f603e9
wendy <wendyrevailler@yahoo.fr>
2021-06-30 14:49:32 +0200

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2021, Salim Kanoun, Wendy Revailler'
 author = 'Salim Kanoun, Wendy Revailler'
 
 # The full version, including alpha/beta/rc tags
-release = '0.72'
+release = '0.75'
 
 
 # -- General configuration ---------------------------------------------------
